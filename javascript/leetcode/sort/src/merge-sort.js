d11e52dcf48410af995206fed2f0d92fd2869641
李亮凯 <1147036010@qq.com>
2021-04-13 09:44:34 +0800

    docs: 二叉树文档完成编写

--- javascript/leetcode/sort/src/merge-sort.js
+++ javascript/leetcode/sort/src/merge-sort.js
@@ -35,5 +35,39 @@ function merge (left = [], right = []) {
   return tmp
 }
 
-// console.log(mergeSort([10, 2, 6, 3, 59, 44, 16, 20, 33, 4, 8, 19, 10, 28]))
+console.log(mergeSort([10, 2, 6, 3, 59, 44, 16, 20, 33, 4, 8, 19, 10, 28]))
 
+/**
+ * 
+ * @param {Number[]} array 
+ */
+function mergeSort2 (array = []) {
+  if(!array.length || array.length === 1) return array
+  const mid = Math.ceil((array.length) / 2)
+  const left = mergeSort2( array.slice( 0, mid ) )
+  const right = mergeSort2( array.slice( mid ) )
+  return merge2(left, right)
+}
+
+function merge2 (left, right) {
+  const result = []
+  while(left.length && right.length) {
+    if(left[0] < right[0]) {
+      result.push(left.shift())
+    } else {
+      result.push(right.shift())
+    }
+  }
+
+  while(left.length) {
+    result.push(left.shift())
+  }
+
+  while(right.length) {
+    result.push(right.shift())
+  }
+
+  return result
+}
+
+console.log( mergeSort2( [ 10, 2, 6, 3, 59, 44, 16, 20, 33, 4, 8, 19, 10, 28 ] ) )
